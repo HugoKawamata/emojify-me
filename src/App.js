5ba5354d791717e3c7becdc13bc37ea7e27eceb5
Hugo Kawamata <hugokawamata@gmail.com>
2017-07-29 23:46:09 +1000

    Style change and minor fixes

--- src/App.js
+++ src/App.js
@@ -11,25 +11,26 @@ class App extends Component {
     this.state = {
       result: [],
       letters: {
-        A: ["00100", "01010", "11111", "10001", "10001", "10001"],
-        B: ["11110", "10001", "11110", "10001", "10001", "11111"],
-        C: ["01111", "10000", "10000", "10000", "10000", "11111"],
-        D: ["11110", "10001", "10001", "10001", "10001", "11110"],
-        E: ["11111", "10000", "11111", "10000", "10000", "11111"]
+        A: ["00100", "01010", "11111", "10001", "10001"],
+        B: ["11110", "10001", "11110", "10001", "11110"],
+        C: ["01111", "10000", "10000", "10000", "01111"],
+        D: ["11110", "10001", "10001", "10001", "11110"],
+        E: ["11111", "10000", "11111", "10000", "11111"],
       }
     }
     this.generate = this.generate.bind(this);
+    this.convert = this.convert.bind(this);
   }
 
   generate(c1, c2, text) {
     let result = [];
     for (let rowI = 0; rowI < 6; rowI++){
       let row = [];
-      row.push(<span key={"row " + rowI + " start"}>0</span>)
+      row.push(<span key={"row " + rowI + " start"}>{c1}</span>)
 
       for (let i = 0; i < text.length; i++){
-        row.push(<span key={"row " + rowI + "letter " + (i + 1)}>{this.state.letters[text[i]][rowI]}</span>);
-        row.push(<span key={"row " + rowI + "letter " + (i + 1) + " end"}>0</span>);
+        row.push(<span key={"row " + rowI + "letter " + (i + 1)}>{this.convert(c1, c2, this.state.letters[text[i]][rowI])}</span>);
+        row.push(<span key={"row " + rowI + "letter " + (i + 1) + " end"}>{c1}</span>);
       }
       row.push(<br key={"row " + rowI + " end" }/>);
       result.push(row)
@@ -37,6 +38,14 @@ class App extends Component {
     }
 
     this.setState({result: result});
+  }
+
+  convert(c1, c2, row) {
+    let newRow = "";
+    for (let i = 0; i < 5; i++) {
+      newRow += (row[i] === "0" ? c1 : c2);
+    }
+    return newRow;
   }
 
   render() {
--- src/Result.js
+++ src/Result.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Result extends React.Component {
     render() {
         return (
-            <div>
+            <div id="result">
                 {this.props.res}
             </div>
         )
